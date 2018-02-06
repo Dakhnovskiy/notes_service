18fd4869e697443e68f0123f4fee429c366eba1a
dezman <dmitriy_dakhnovskiy@mail.ru>
2018-02-06 21:22:35 +0300

    add filters and permissions

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from . import models
 from . import serializers
@@ -6,6 +6,17 @@ from . import serializers
 
 class NoteViewSet(viewsets.ModelViewSet):
 
-    queryset = models.Note.objects.all()
     serializer_class = serializers.NoteSerializer
-    # permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        outer_user_id = self.request.query_params.get('outer_user_id')
+        objects = models.Note.objects.filter(user=self.request.user)
+
+        if outer_user_id:
+            objects = objects.filter(outer_user_id=outer_user_id)
+
+        return objects
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
