a8dd8d5ef6fdc15c153125a4426f2623f6b2a438
dezman <dmitriy_dakhnovskiy@mail.ru>
2018-02-06 21:20:59 +0300

    add user

--- notes/models.py
+++ notes/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Note(models.Model):
     title = models.CharField(max_length=256)
     text = models.TextField()
     outer_user_id = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
